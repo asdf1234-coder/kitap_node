b59c26f9b604282fb12bf60b133853a7942cf4f3
asdf <penspinning222@gmail.com>
2024-09-05 07:18:36 +0300

    save

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ const connection = mysql.createConnection({
     database: 'kitap_php'
 });
 connection.query('SELECT * FROM kitaplar', (error, results, fields) => {
-    /*app.use("/hayvan/:isim", function(req, res){
-        res.send(req.params.isim);
-    })*/
     app.use("/kitap/:id", function(req, res){
-        //const urun = data.find(u => )
         const x = req.params.id
         res.render("kitap_detay",{
             url: x,
