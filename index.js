b55437e20282a32aa2271f15691b247c6ce6c60c
asdf <penspinning222@gmail.com>
2024-09-11 22:12:26 +0300

    save

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const mysql = require("mysql2")
+// const mysql = require("mysql2")
+const mongoose = require('mongoose')
 let data = []
 const express = require("express")
+const http = require('http');
+const net = require('net');
 const app = express();
 const path = require("path")
 app.set("view engine","ejs");
 app.use('/img', express.static(path.join(__dirname, 'img')));
-const connection = mysql.createConnection({
+require('dotenv').config();
+
+// Bağlantı URI'nız
+const dbUri = process.env.DB_URI;
+/*const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
@@ -29,4 +36,72 @@ connection.query('SELECT * FROM kitaplar', (error, results, fields) => {
         console.log("dinleniyor")
     })
 });
-connection.end();
+connection.end();*/
+function findAvailablePort(startPort, callback) {
+  const server = net.createServer();
+  
+  server.listen(startPort, () => {
+    server.once('close', () => callback(startPort));
+    server.close();
+  });
+  
+  server.on('error', () => {
+    findAvailablePort(startPort + 1, callback);
+  });
+}
+const initialPort = parseInt(3000);
+mongoose.connect(dbUri, {
+})
+.then(() => {
+  console.log('MongoDB bağlantısı başarılı!');
+  
+  const KullaniciSchema = new mongoose.Schema({
+    id: Number,
+    isim: String,
+    resim: String,
+    yayinevi: String,
+    tur: String,
+    yazar: String,
+    ucret: Number
+  });
+
+  const kitaps = mongoose.model('Kullanici', KullaniciSchema, 'kitaplar');
+
+  kitaps.find({})
+    .then((veriler) => {
+      if (veriler.length === 0) {
+        console.log('Kullanıcılar koleksiyonunda veri bulunmuyor.');
+      } else {
+        app.use("/kitap/:id", function(req, res){
+            const x = req.params.id
+            res.render("kitap_detay",{
+                url: x,
+                kitaplar: veriler
+            })
+        })
+        app.use("/anasayfa", function(req, res){
+            res.render("anasayfa",{
+                kitaplar: veriler
+            });
+        })
+        // Çevre değişkeninden port numarasını al, eğer varsa
+        findAvailablePort(initialPort, (port) => {
+          app.listen(port, () => {
+            console.log(`dinleniyor port: ${port}`);
+          });
+        });
+        /*app.listen(0, () => {
+            console.log(`dinleniyor port: ${port}`)
+        })*/
+        // console.log('Kullanıcılar:', veriler[0]['isim']);
+      }
+      mongoose.connection.close();
+    })
+    .catch((error) => {
+      console.error('Veri çekme hatası:', error);
+      mongoose.connection.close();
+    });
+})
+.catch((error) => {
+  console.error('MongoDB bağlantı hatası:', error);
+});
