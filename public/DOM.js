918bae9b6b4ae8aaf1df130d1402386a6ce7c39a
asdf <penspinning222@gmail.com>
2024-09-15 08:15:01 +0300

    number input input event

--- public/DOM.js
+++ public/DOM.js
@@ -4,73 +4,6 @@ const yazar = document.getElementById("yazar")
 const min = document.getElementById("min")
 const max = document.getElementById("max")
 const ana = document.querySelectorAll(".ana")
-
-/*function filtre(){
-    const tucontrol = ""
-    const yazcontrol = ""
-    const yaycontrol = ""
-    let yay = yayinevi.value
-    let tu = tur.value
-    let yaz = yazar.value
-    let mi = min.value
-    let ma = max.value
-    if(ma === ""){
-        ma = 9999999;
-        console.log("girdi")
-    }
-    if(mi === ""){
-        mi = 0;
-    }
-    if(yay === ""){
-        const yaycontrol = "bos"
-    }
-    if(tu === ""){
-        const tucontrol = "bos"
-    }
-    if(yaz === ""){
-        const yazcontrol = "bos"
-    }
-    ana.forEach(card => {
-        if(yay === ""){
-            yay = card.querySelector(".card-yayin").textContent
-        }
-        if(tu === ""){
-            tu = card.querySelector(".card-tur").textContent
-        }
-        if(yaz === ""){
-            yaz = card.querySelector(".card-yazar").textContent
-        }
-        if((parseInt(card.querySelector(".card-tutar").textContent) > parseInt(ma)) || (parseInt(card.querySelector(".card-tutar").textContent) < parseInt(mi))){
-
-            card.style.display = 'none';
-            /*console.log(typeof ma)
-            console.log(typeof mi)
-            console.log(`ma: ${ma}`)
-            console.log(`mi: ${mi}`)
-            // console.log(`cart tutar (${parseInt(card.querySelector(".card-tutar").textContent)}) max değerden büyük (${ma})`)
-        }else{
-            card.style.display = '';
-        }
-        if(yay !== card.querySelector(".card-yayin").textContent){
-            card.style.display = 'none';
-        }
-        if(tu !== card.querySelector(".card-tur").textContent){
-            card.style.display = 'none'
-        }
-        if(yaz !== card.querySelector(".card-yazar").textContent){
-            card.style.display = 'none'
-        }
-        if(yaycontrol === "bos"){
-            yay = ""
-        }
-        if(yazcontrol === "bos"){
-            yaz = ""
-        }
-        if(tucontrol === "bos"){
-            tu = ""
-        }
-    });
-}*/
 function filtre() {
     let yay = yayinevi.value || "";
     let tu = tur.value || "";
@@ -94,12 +27,11 @@ function filtre() {
         }
     });
 }
-
 function addEvent(){
     yayinevi.addEventListener('keyup', filtre);
     tur.addEventListener('keyup', filtre);
     yazar.addEventListener('keyup', filtre);
-    min.addEventListener('keyup', filtre);
-    max.addEventListener('keyup', filtre);
+    min.addEventListener('input', filtre);
+    max.addEventListener('input', filtre);
 }
 addEvent()
